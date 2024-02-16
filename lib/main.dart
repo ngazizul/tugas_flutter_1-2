655b7707c9f12f8b125f09981c90e178babc5a5a
unknown <azmingazizul@gmail.com>
2024-02-16 22:48:36 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:aplikasi_flutter_pertamaku/hello_world.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext content) {
+    return MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: HelloWorld(),
+);
+}
+}
